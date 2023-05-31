6ba0acf81e55fe1fb0dcee3fa3c3b2b58814cdf0
ashishzyod12390 <ashish.chaursiya@zyod.com>
2023-05-31 21:40:30 +0530

    base code with build file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v8.0.35',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl:'https://urvuapp.azurewebsites.net/api',
+  apiUrl:'http://24.199.104.72/api',
   appThemeName: 'ShopinKart',
   appPurchaseUrl: 'https://1.envato.market/EA4JP',
   appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/demo1/documentation/base/helpers/flex-layouts.html',
